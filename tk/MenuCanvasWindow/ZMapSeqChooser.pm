cd63120276d92957dd6b9ddc8229796197c12de2
rds <rds>
2005-06-23 16:33:59 +0000

    - uses ConnectUtils more - added error stuff - ...

--- tk/MenuCanvasWindow/ZMapSeqChooser.pm
+++ tk/MenuCanvasWindow/ZMapSeqChooser.pm
@@ -8,6 +8,7 @@ use MenuCanvasWindow::SeqChooser;
 use ZMap::Connect qw(:all);
 use Sys::Hostname;
 use Tie::Watch;
+use Hum::Ace::LocalServer;
 
 our @ISA = qw(MenuCanvasWindow::SeqChooser);
 
@@ -49,28 +50,34 @@ sub launch {
     my( $self ) = @_;
     
     $self->kill;
-    my $z = $self->insert_zmap_connector();
-    my $s_id = $z->server_window_id();
     if (my $path = $self->ace_path) {
-        if($self->spawn_sgifaceserver){
+        if(my $ok = $self->spawn_sgifaceserver){
+            my $z = $self->insert_zmap_connector();
             $self->write_dot_zmap();
-            sleep(2);
-            if (my $pid = fork) {
-                print STDERR " zmap forked\n";
-                $self->process_id_list($pid);
-            }
-            elsif (defined($pid)) {
-                my $c = $self->zmap_dir();
-                my @e = ('zmap', '--conf_dir', $c, '--win_id', $s_id);
-                print  "I ran '@e' \n";
-                exec( @e );
-            }
-            else {
-                confess "Error: can't fork : $!";
-            }
 
+            my @e = ('zmapq', 
+                     '--conf_dir' => $self->zmap_dir,
+                     '--win_id'   => $z->server_window_id);
+
+            sleep(2);
+            my $ref = Hum::Ace::LocalServer::full_child_info();
+            my $pid = fork_exec(\@e, $ref, 0, sub { 
+                my ($info) = @_;
+                flush_bad_windows;
+#                use Data::Dumper;                
+#                warn "INFO: ", Dumper($info);
+            });
+            if($pid){
+                $self->process_id_list($pid);
+            }else{
+                my $mess = "Error: couldn't fork()\n";
+                warn $mess;
+                $self->message($mess);
+            }
         } else {
-             warn "Error: couldn't spawn sgifaceserver for some reason...";
+            my $mess = "Error: couldn't spawn sgifaceserver for some reason...";
+            warn $mess;
+            $self->message($mess);
         }
     } else {
         warn "Error: ace_path not set";
@@ -132,6 +139,9 @@ sub DESTROY {
     # need to undef AceDatabase for it's clean up.
     # warn "AceDatabase->unlock should now happen\n";
     $self->kill(); # get rid of zmap
+    # clear xclient objects
+    flush_bad_windows();
+
     delete $self->{'_SGIF_LOCAL_SERVER'}; # shutdown server
     delete $self->{'_AceDatabase'}; # unlock will now happen
     # warn "AceDatabase->unlock should have happened\n";
@@ -154,27 +164,29 @@ sub DESTROY {
 
 sub insert_zmap_connector{
     my ($self) = @_;
-    my $justOneInstance = 
     my $zc = $self->{'_zmap_connector'};
     if(!$zc){
+        my $mb   = $self->menu_bar();
         my $zmap = ZMap::Connect->new( -server => 1 );
-        $zmap->init($self->menu_bar(), \&RECEIVE_FILTER, [ $self, qw( register_client ) ]);
+        $zmap->init($mb, \&RECEIVE_FILTER, [ $self, qw( register_client ) ]);
         my $id = $zmap->server_window_id();
         $zc = $self->{'_zmap_connector'} = $zmap;
     }
     return $zc;
 }
-
+sub zmap_connector{
+    return shift->insert_zmap_connector(@_);
+}
 sub spawn_sgifaceserver{
     my ($self) = @_;
     my $sgif   = $self->{'_SGIF_LOCAL_SERVER'};
     my $path   = $self->ace_path();
     unless($sgif){
         $sgif = Hum::Ace::LocalServer->new($path);
-        $sgif->server_executable("/nfs/team71/acedb/edgrif/acedb/CODE/acedb/bin.ALPHA_5/sgifaceserver");
+        $sgif->server_executable('sgifaceserver');
         $sgif->timeout_string('0:0');
         $sgif->start_server() or return 0; # this only check the fork was successful
-        $sgif->ace_handle     or return 0; # this checks it can connect
+        $sgif->ace_handle(1)  or return 0; # this checks it can connect
         $self->zmap_port($sgif->port);
         $self->{'_SGIF_LOCAL_SERVER'} = $sgif;
     }
@@ -243,21 +255,22 @@ sub zmap_dir {
 }
 
 
+#===========================================================
 
 sub RECEIVE_FILTER{
     my ($_connect, $_request, $_obj, @list) = @_;
     my ($_status, $_response) 
-        = (404, "<error>unknown command</error>");
+        = (404, $_obj->zmap_connector->basic_error("Unknown Command"));
 
     foreach my $valid(@list){
-        if($_request =~ s/^$valid//){
+        if($_request =~ s/^$valid// && $_obj->can($valid)){
             ($_status, $_response) 
-                = $_obj->$valid($_request) if $_obj->can($valid);
+                = $_obj->$valid($_request);
             last;
         }
     }
-
-    return ($_status, $_response) ;    
+#    warn  ($_status, ":",$_response) ;
+    return ($_status, $_response) ;
 }
 
 sub register_client{
@@ -265,8 +278,11 @@ sub register_client{
 
     my $p  = parse_params($request);
     my $xr = xclient_with_name('main');
+    my $z  = $self->zmap_connector();
 
-    return if $xr;
+    $self->entry_ref();
+
+    return (200, $z->basic_error("ZMap already Exists")) if $xr;
 
     unless($p->{'id'} 
            && $p->{'request'}
@@ -274,10 +290,10 @@ sub register_client{
         warn "mismatched request for register_client:\n", 
         "id, request and response required\n",
         "Got '$request'\n";
-        return (403, "<error>Bad request!</error>");
+        return (403, $z->basic_error("Bad Request!"));
     }
  
-    $xr = xclient_with_name('main', $p->{'id'});
+    xclient_with_name('main', $p->{'id'});
 
     $self->set_entry_value('main');
 
@@ -286,7 +302,7 @@ sub register_client{
                     -store    => [ \&open_clones, $self ],
                     );
 
-    return (200, "<error>Bingo.</error>");
+    return (200, $z->basic_error(q`no error, just easier...`));
 }
 
 sub open_clones{
@@ -298,35 +314,95 @@ sub open_clones{
     my $cmd = "newZmap seq = @clones ; start = 1 ; end = 0";
 
     $self->make_request($cmd);
+
+    my $windows = $self->make_menu('ZMap',1);
+    foreach my $name (list_xclient_names()){
+        $windows->add('command',
+                      -label   => $name,
+                      -command => sub { 
+                          $self->set_entry_value($name); 
+                          $self->make_request('zoom_in');
+                      },
+                      -accelerator => 'Ctrl+K',
+                      -underline   => 0
+                      );
+    }
     
     $watch->Unwatch;
 }
 
+#===========================================================
+
 sub make_request{
-    my ($self, @commands) = @_;
+    my ($self, $handler, @commands) = @_;
 
     my $xr = $self->current_xclient;
     unless($xr){
         warn "No current window.";
-        return;
+        return ;
     }
+    my $type = ref($handler);
+    unless($type && $type eq 'CODE'){
+        unshift(@commands, $handler) if !$type;
+        $handler = undef;
+    }
+    $handler  ||= \&RESPONSE_HANDLER;
+    my $error ||= \&ERROR_HANDLER;
 
     my @a = $xr->send_commands(@commands);
-    my $canvasMessage = "Sent commands\n";
+
     for(my $i = 0; $i < @commands; $i++){
-        $canvasMessage .= " - cmd: '" . $commands[$i] . "',  result: '" . $a[$i] . "'\n";
-        my ($status, $n) = parse_response($a[$i]);
-        my ($name, $id) = ($n->{zmapid}, $n->{windowid});
-        if($status == 412){
-            delete_xclient_with_id($n->{'error'}->{'windowid'});
-        }
-        if($name){
-            xclient_with_name($name, $id) if $id;
-            $self->set_entry_value($name);
+        my ($status, $xmlHash) = parse_response($a[$i]);
+        if($status =~ /^2\d\d/){ # 200s
+            $handler->($self, $xmlHash);
+        }else{
+            $error->($self, $status, $xmlHash);
         }
     }
-    warn $canvasMessage;
+    return ;
 }
+sub RESPONSE_HANDLER{
+    my ($self, $xml) = @_;
+    warn "In RESPONSE_HANDLER\n";
+    my ($name, $id) = ($xml->{zmapid}, $xml->{windowid});
+    if($name){
+        xclient_with_name($name, $id) if $id;
+        $self->set_entry_value($name);
+    }
+    return ;
+}
+sub ERROR_HANDLER{
+    my ($self, $status, $xml) = @_;
+    $xml = $xml->{'error'}; # this is all we care about
+    warn "In ERROR_HANDLER\n";
+    if($status == 400){
+
+    }elsif($status == 401){
+
+    }elsif($status == 402){
+
+    }elsif($status == 403){
+
+    }elsif($status == 404){
+        # could do something clever here so that we don't send the same window this command again.
+        my $message = $xml->{'message'};
+    }elsif($status == 412){
+        delete_xclient_with_id($xml->{'windowid'});
+    }elsif($status == 500){
+    
+    }elsif($status == 501){
+    
+    }elsif($status == 502){
+    
+    }elsif($status == 503){
+    
+    }else{
+        warn "I know nothing about status $status\n";
+    }
+    return ;
+}
+
+#===========================================================
 
 sub current_xclient{
     my ($self) = @_;
